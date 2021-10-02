fda3e013203f521964694b9bfabfbac8b4caa8dc
charasullia <charanrajr713@gmail.com>
2021-10-02 12:27:29 +0530

    [charansullia]added:welcome message

---
+++ AdressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to adressbook computation");
+        }
+    }
+}
